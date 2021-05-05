4dae35aef74c5b7a4c7a6232c3e095f7e2fd9d55
David2508 <deividrocan2508@gmail.com>
2021-05-05 09:33:46 -0500

    Checando Imagenes :(

---
+++ categorias.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title> Categorias </title>
+        <link rel="stylesheet" href="css/styles.css">
+        <meta name="viewport" content="width =device-width, initial-scale =1"
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&family=Kaushan+Script&family=Secular+One&display=swap" rel="stylesheet">
+    </head>
+    <body>
+
+    <section id=".bannerCat" >
+        <h1>Prueba de conexión</h1>
+    </section>
+<?php
+    
+    ?>
+</body>
+
+    </body>
+</html>
+
+
+
