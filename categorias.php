a70c4691b72c6b7781572f6fd7efd37990fccc06
Motaiv <koke0mota@gmail.com>
2021-05-12 11:14:11 -0500

    cambios prueba bd

--- categorias.php
+++ categorias.php
@@ -4,7 +4,8 @@
 <head>
     <title> Categorias </title>
     <link rel="stylesheet" href="css/styles.css">
-    <meta name="viewport" content="width =device-width, initial-scale =1" <link rel="preconnect" href="https://fonts.gstatic.com">
+    <meta name="viewport" content="width =device-width, initial-scale =1"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&family=Kaushan+Script&family=Secular+One&display=swap" rel="stylesheet">
 </head>
      <?php
